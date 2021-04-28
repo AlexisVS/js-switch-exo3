c79eaa22fc570ebefe7755c743fc9c514f5a20f9
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-28 20:38:38 +0200

    Add files via upload

---
+++ js-switch-3/main.js
@@ -0,0 +1,14 @@
+let x = +prompt("dfbdfgfd");
+switch (x % 2) {
+    case (0):
+        alert("ton nombre est divisible par 2");
+        break;
+    case (1):
+        alert("ton nombre est pas divisible par 2");
+        break;
+
+    default:
+        alert("Tu as entré des lettres");
+        console.log(x);
+        break;
+}
